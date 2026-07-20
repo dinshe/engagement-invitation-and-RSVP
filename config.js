031a0a06bc5a41d2b5e9b6e0f63c3643427f5a7b
dinshe <121294667+dinshe@users.noreply.github.com>
2026-07-20 21:40:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Paste the deployed Google Apps Script Web App URL below.
 // Keep this file beside index.html and admin.html.
 window.INVITATION_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx1dOeaotwdVoKyOECLqeFiqht4Yn-GRnzq50I2Eqvh8EbJDWvKITmb4eJiUWCbGR9fWw/exec"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxay3QExf2O4CbrzCQg5ejF-gSRJmWL-ykpKhXMiM-L5sEpEbLFWcyLmT_QwIsVq0VKYw/exec"
 };
